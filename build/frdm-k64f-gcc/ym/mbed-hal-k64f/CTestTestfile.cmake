3ded775d2798239e9a3726ecb89d88edcfac7dd1
Doug Anson <doug.anson@arm.com>
2016-02-21 10:44:33 -0600

    updated

--- build/frdm-k64f-gcc/ym/mbed-hal-k64f/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/douans01/Documents/workspace/GitHub/yotta_projects/mbed-ethernet-sample/build/frdm-k64f-gcc/ym/mbed-hal-k64f
-# Build directory: /Users/douans01/Documents/workspace/GitHub/yotta_projects/mbed-ethernet-sample/build/frdm-k64f-gcc/ym/mbed-hal-k64f
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(../mbed-hal-frdm-k64f)
-subdirs(source)
