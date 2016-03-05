7fec24d5253d93d4dde2b7b7713eab1954cc88b3
Doug Anson <doug.anson@arm.com>
2016-03-05 00:03:59 +0000

    updated

--- build/frdm-k64f-gcc/ym/mbed-hal-ksdk-mcu/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/douans01/Documents/workspace/GitHub/yotta_projects/mbed-ethernet-sample/build/frdm-k64f-gcc/ym/mbed-hal-ksdk-mcu
-# Build directory: /Users/douans01/Documents/workspace/GitHub/yotta_projects/mbed-ethernet-sample/build/frdm-k64f-gcc/ym/mbed-hal-ksdk-mcu
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(../uvisor-lib)
-subdirs(../mbed-hal-k64f)
-subdirs(source)
--- build/frdm-k64f-gcc/ym/unity/source/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/douans01/Documents/workspace/GitHub/yotta_projects/mbed-ethernet-sample/build/frdm-k64f-gcc/ym/unity/source
-# Build directory: /Users/douans01/Documents/workspace/GitHub/yotta_projects/mbed-ethernet-sample/build/frdm-k64f-gcc/ym/unity/source
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
