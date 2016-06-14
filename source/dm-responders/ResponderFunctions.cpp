72dd08a30d6ac80c194550ec356e0a6533bd113c
Doug Anson <doug.anson@arm.com>
2016-06-14 16:12:32 -0500

    updated

--- source/dm-responders/ResponderFunctions.cpp
+++ source/dm-responders/ResponderFunctions.cpp
@@ -29,12 +29,6 @@
 // DeviceManagementResponder Support
 #include "mbed-connector-interface/DeviceManagementResponder.h"
 
-// DeviceFirmwareCompositeResource Support
-#include "mbed-connector-interface/DeviceFirmwareCompositeResource.h"
-
-// DeviceIntegerResource Support
-#include "mbed-connector-interface/DeviceIntegerResource.h"
-
 // NVIC System Reset
 extern "C" void NVIC_SystemReset(void);
 
@@ -50,14 +44,11 @@ extern "C" bool dm_reboot_responder(const void *e,const void *l,const void */* n
     Connector::Endpoint *ep = (Connector::Endpoint *)e;
     
     // Device Manager
-    DeviceManager *dm = (DeviceManager *)ep->getDeviceManager();
+    //DeviceManager *dm = (DeviceManager *)ep->getDeviceManager();
     
     // DeviceManagementResponder
-    DeviceManagementResponder *dmr = (DeviceManagementResponder *)dm->getResponder();
-    
-    // FirmwareComposite Resource
-    DeviceFirmwareCompositeResource *fcr = (DeviceFirmwareCompositeResource *)dmr->getFirmwareCompositeResource();
-    
+    // DeviceManagementResponder *dmr = (DeviceManagementResponder *)dm->getResponder();
+        
     // DEBUG
     logger->log("dm_reboot_responder: de-registering endpoint...");
 
@@ -83,14 +74,11 @@ extern "C" bool dm_reset_responder(const void *e,const void *l,const void */* no
     Connector::Endpoint *ep = (Connector::Endpoint *)e;
     
     // Device Manager
-    DeviceManager *dm = (DeviceManager *)ep->getDeviceManager();
+    //DeviceManager *dm = (DeviceManager *)ep->getDeviceManager();
     
     // DeviceManagementResponder
-    DeviceManagementResponder *dmr = (DeviceManagementResponder *)dm->getResponder();
-    
-    // FirmwareComposite Resource
-    DeviceFirmwareCompositeResource *fcr = (DeviceFirmwareCompositeResource *)dmr->getFirmwareCompositeResource();
-    
+    //DeviceManagementResponder *dmr = (DeviceManagementResponder *)dm->getResponder();
+        
     // DEBUG
     logger->log("dm_reset_responder: de-registering endpoint...");
 
@@ -117,17 +105,7 @@ extern "C" bool dm_invoke_fota(const void *e,const void *l) {
     Connector::Endpoint *ep = (Connector::Endpoint *)e;
     
     // Device Manager
-    DeviceManager *dm = (DeviceManager *)ep->getDeviceManager();
-    
-    // DeviceManagementResponder
-    DeviceManagementResponder *dmr = (DeviceManagementResponder *)dm->getResponder();
-    
-    // FirmwareComposite Resource
-    DeviceFirmwareCompositeResource *fcr = (DeviceFirmwareCompositeResource *)dmr->getFirmwareCompositeResource();
-    
-    // State and Result Resources
-    DeviceIntegerResource *state = (DeviceIntegerResource *)fcr->getResource(LWM2M_STATE_ID);
-    DeviceIntegerResource *result = (DeviceIntegerResource *)fcr->getResource(LWM2M_RESULT_ID);
+    //DeviceManager *dm = (DeviceManager *)ep->getDeviceManager();
     
     // DEBUG
     logger->log("fota_update: de-registering endpoint...");
@@ -139,17 +117,12 @@ extern "C" bool dm_invoke_fota(const void *e,const void *l) {
     logger->log("fota_update: invoking FOTA on the endpoint...");
     
     // Download
-    result->put("0");   // FOTA beginning
-    state->put("2");    // we are downloading... 
     logger->log("fota_update: FOTA beginning... Downloading...");
     
     // DO THE FOTA update (FUTURE)
-    state->put("3"); // download completed... FOTA...
     logger->log("fota_update: Download completed. Invoking FOTA on device...");
     
     // Clean up and reboot
-    state->put("1");    // idle
-    result->put("1");   // FOTA completed successfully 
     logger->log("fota_update: FOTA completed. Rebooting endpoint...");
     
     // return OK
